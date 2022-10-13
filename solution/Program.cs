4a42675a87aa9d470f58b9fd4fb07263840f2f94
Valentina V. Trunova <valentina.v.trunova@yandex.ru>
2022-10-13 17:10:48 +0700

    написала и протестировала код

--- solution/Program.cs
+++ solution/Program.cs
@@ -4,11 +4,14 @@ string[] array = AskArray();
 string[] result = FindLessThan(array, 3);
 Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
 
-string[] FindLessThan(string[] input, int n) {
+string[] FindLessThan(string[] input, int n)
+{
     string[] output = new string[CountLessThan(input, n)];
 
-    for(int i = 0, j = 0; i < input.Length; i++) {
-        if(input[i].Length <= n) {
+    for (int i = 0, j = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
             output[j] = input[i];
             j++;
         }
@@ -17,11 +20,14 @@ string[] FindLessThan(string[] input, int n) {
     return output;
 }
 
-int CountLessThan(string[] input, int n) {
+int CountLessThan(string[] input, int n)
+{
     int count = 0;
 
-    for(int i = 0; i < input.Length; i++) {
-        if(input[i].Length <= n) {
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
             count++;
         }
     }
@@ -29,7 +35,8 @@ int CountLessThan(string[] input, int n) {
     return count;
 }
 
-string[] AskArray() {
+string[] AskArray()
+{
     Console.WriteLine("Введите значения через пробел: ");
     return Console.ReadLine().Split(" ");
 }
